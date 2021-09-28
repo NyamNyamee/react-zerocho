c874a93b75de8107f9ead48a1c031a0b9a03dac0
inwoo <jopelee2@gmail.com>
2021-09-28 17:57:25 +0900

    노드, 웹팩 사용 테스트

---
+++ lecture/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');  // 노드 모듈
+
+module.exports = {
+    name: 'word-relay-setting', // 이름은 마음대로
+    // mode: 'production', 상용
+    mode: 'development', // 개발
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']  // 배열에 확장자를 입력해주면 아래 엔트리에서 파일 확장자를 적지 않아도 됨
+    },
+
+    entry: { // 입력
+        app: ['./client'],  // 배열 안의 파일을 합쳐서 아래에서 지정한 파일로 출력해준다
+    },
+    output: { // 출력
+        path: path.join(__dirname, 'dist'),  // 현재폴더인 lecture 하위의 dist폴더를 출력 폴더로 지정
+        filename: 'app.js'
+    },
+};
